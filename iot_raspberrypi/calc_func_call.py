08e05e2fde56628dd373f2fa3066f2c24b832b95
JunHyen Mun <impelfin@gmail.com>
2020-12-02 20:20:07 +0900

    no message

--- iot_raspberrypi/calc_func_call.py
+++
@@ -1,7 +0,0 @@
-from calc import *
-
-a = int(input('Input First Number : '))
-b = int(input('Input Second Number : '))
-
-print('{} + {} = {} '.format(a, b, add(a, b)))
-print('{} - {} = {} '.format(a, b, sub(a, b)))
